9b0073c3495e25e06fdb2688548c4a683e827825
TIMTOMTOP <108585960+TIMTOMTOP@users.noreply.github.com>
2022-08-18 09:42:41 +0300

    Update RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -13,7 +13,6 @@ import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
-
 df=pd.read_csv('data.csv')
 
 # Quantifying the charges with quantiles
@@ -28,8 +27,7 @@ df.loc[df['sex']=='female','sex'],df.loc[df['sex']=='male','sex']=1,0
 df.loc[df['region']=='southwest','region'],df.loc[df['region']=='southeast','region'],df.loc[df['region']=='northwest','region'],df.loc[df['region']=='northeast','region']=1,2,3,4
 df.loc[df['smoker']=='yes','smoker'],df.loc[df['smoker']=='no','smoker']=1,0
 
-
-df=pd.concat([df,df,df]) # Increasing the data set
+df=pd.concat([df,df,df,df,df,df,df]) # Increasing the data set
 df=df.reset_index(drop=True)
 
 label=df['charges'] # labels
@@ -41,7 +39,7 @@ Y=label
 
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.8,random_state=42)
 
-model = RandomForestClassifier(n_estimators=200, random_state=42) # creating the model
+model = RandomForestClassifier(n_estimators=1000, random_state=42) # creating the model
 model.fit(X_train,Y_train) # Fitting the model
 ypred = model.predict(X_test)
 print(metrics.classification_report(ypred, Y_test))
@@ -53,5 +51,6 @@ plt.xlabel('true label')
 plt.ylabel('predicted label')
 plt.show()
 
-
-
+Square_error=list((ypred-Y_test)**2)
+mean_square=sum(Square_error)/len(Square_error)
+print(mean_square)
